a6e9418e085bd0710febcbc58d1c84e6e1038290
freeman <iviewn@gmail.com>
2022-08-28 14:25:51 +0300

    sugar

--- pages/index.js
+++ pages/index.js
@@ -159,3 +159,12 @@ function openSubmitPopup(bool) {
         }
         
     });
+
+	$('.header__menu-link').on('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top - 20
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+        //remove_hash_from_url();
+    });
